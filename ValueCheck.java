2b33e46c31e4e3d7273074293caa060c989f0089
Pavlo-Andrianatos <andpavlo@gmail.com>
2020-05-18 14:56:27 +0200

    More fixes for value checking

---
+++ ValueCheck.java
@@ -0,0 +1,64 @@
+package MyPackage;
+
+// Author: Constantinos Pavlo Andrianatos
+// Student number: 17173389
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class ValueCheck {
+
+    Node tree;
+
+    ValueCheck(String inputName){
+        TypeCheck typeCheck = new TypeCheck(inputName);
+
+        tree = typeCheck.getTree();
+
+        /*if(tree.setDefiniteValues()){
+            System.out.println("Value Errors detected, please fix and run compiler again.");
+            System.exit(0);
+        } else{
+            System.out.println("Value Checking completed successfully.");
+        }*/
+
+        boolean checkOne = tree.setDefiniteValues();
+        boolean checkTwo = tree.valueChecks();
+
+        if(checkOne || checkTwo){
+            System.out.println("Value Errors detected, please fix and run compiler again.");
+            System.exit(0);
+        } else{
+            System.out.println("Value Checking completed successfully. Generated ValueCheck.txt with details.");
+        }
+
+        tree.reassignSymbolTable();
+
+        //tree.justPrint("", false);
+
+        try {
+            File fileOutput = new File("ValueCheck.txt");
+            FileWriter writer = new FileWriter(fileOutput);
+
+            tree.print("", false, writer);
+            writer.flush();
+            writer.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public Node getTree(){
+        return tree;
+    }
+
+    public static void main(String[] args) {
+        //First tested input file
+        String inputName = "g.txt";
+        new ValueCheck(inputName);
+
+        //Second tested input file
+        //String inputName = "input1.txt";
+        //new ValueCheck(inputName);
+    }
+}
